6414ecb0b9a382d26ad1ea02bc12fb9136d09e10
PeiSong Xiong <peisongo@gmail.com>
2017-03-20 18:27:21 +0800

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+/*
+ * Angular 2 decorators and services
+ */
+import {
+  Component,
+  OnInit,
+  ViewEncapsulation
+} from '@angular/core';
+import { AppState } from './app.service';
+import * as _ from 'lodash';
+
+/*
+ * App Component
+ * Top Level Component
+ */
+@Component({
+  selector: 'app',
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: [
+    './app.component.css'
+  ],
+  template: `
+    <h1>Hello World</h1>
+    <router-outlet></router-outlet>
+  `
+})
+export class AppComponent implements OnInit {
+  public name = 'app.rest.studio';
+
+  constructor(
+    public appState: AppState
+  ) {}
+
+  public ngOnInit() {
+    console.log('Initial App State', this.appState.state, _.trim(null));
+  }
+
+}
+
+/*
+ * Please review the https://github.com/AngularClass/angular2-examples/ repo for
+ * more angular app examples that you may copy/paste
+ * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
+ * For help or questions please contact us at @AngularClass on twitter
+ * or our chat on Slack at https://AngularClass.com/slack-join
+ */
---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { NoContentComponent } from './no-content';
+
+export const ROUTES: Routes = [
+    { path: '**', component: NoContentComponent },
+];
